2d6b805ec5a42a213d818f69543304537d16bfd7
Chukwudi Ochade <chukwudiochade@gmail.com>
2025-07-21 21:17:10 +0100

    updating the todo app

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -4,21 +4,72 @@ import "./App.css";
 import { CommonModal } from "./components/Modal";
 import { useState } from "react";
 function App() {
+  const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
   const [openModal, setOpenModal] = useState(false);
   const [textInput, setTextInput] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(savedTasks);
   const [time, setTime] = useState("");
+  const [statusCheck, setStatusCheck] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  console.log(todos);
+  console.log(savedTasks);
+  console.log(statusCheck);
 
   function addTodo() {
+    if (textInput.length < 2) {
+      return alert("Title cannot be less than 2 characters long");
+    }
     const task = {
       id: Date.now(),
       title: textInput,
-      status: "pending",
+      isCompleted: false,
+      startDate: "",
     };
-    setTodos((prev) => [...prev, task]);
+    setTodos([...todos, task]);
+    localStorage.setItem("tasks", JSON.stringify([...todos, task]));
+
     handleCloseModal();
     setTextInput("");
   }
+  function handleMarkAsCompleted(title) {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.title === title) {
+        console.log({ todo });
+        return { ...todo, isCompleted: !todo.isCompleted };
+
+        // setTodos((prev) => [...prev, { ...todo, status: "completed" }]);
+      } else {
+        return todo;
+      }
+
+      console.log({ todo });
+    });
+    setTodos(updatedTodos);
+  }
+  function handleRemoveAllTodos() {
+    // localStorage.removeItem("tasks");
+
+    setTodos([]);
+    localStorage.setItem("tasks", JSON.stringify([]));
+  }
+  function handleRemoveTodo(title) {
+    const filterTodos = todos.filter((todo) => todo.title !== title);
+    setTodos(filterTodos);
+    localStorage.setItem("tasks", JSON.stringify(filterTodos));
+  }
+  console.log(todos);
+  function handleChangeDate(title, date) {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.title === title) {
+        return { ...todo, startDate: date };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(updatedTodos);
+  }
+
   // const todos = [
   //   {
   //     id: 1,
@@ -32,14 +83,17 @@ function App() {
   //   },
   // ];
 
+  const filteredTodos = todos.filter((todo) => {
+    return todo.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   function handleOpenModal() {
     setOpenModal(true);
   }
+
   function handleCloseModal() {
     setOpenModal(false);
   }
-  console.log(openModal);
-  console.log(textInput);
   return (
     <main>
       <section className="todo">
@@ -47,13 +101,16 @@ function App() {
 
         <div className="comment-container">
           <div id="comment">
-            <textarea
+            <input
               rows="1"
               name="comment"
               className="comment-search"
               placeholder="Search note..."
-            ></textarea>
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
           </div>
+
           <div className="btn-all">
             <button className="btn">All</button>
           </div>
@@ -64,30 +121,51 @@ function App() {
         </div>
       </section>
       <section className="note">
-        {todos.map((todo, index) => (
-          <div key={todo.id}>
-            <div className="note1">
-              <div>
-                <input
-                  name="note2"
-                  id="note2"
-                  className="checkbox"
-                  type="checkbox"
-                />
+        {filteredTodos.map((todo, index) => (
+          <div key={todo.id} className="note-section">
+            <div className="note-flex">
+              <div className="note1">
+                <div>
+                  <input
+                    name="note2"
+                    id="note2"
+                    className="checkbox"
+                    type="checkbox"
+                    value={todo.title}
+                    onChange={(event) =>
+                      handleMarkAsCompleted(event.target.value)
+                    }
+                  />
+                </div>
+                <label
+                  htmlFor="note2"
+                  className={
+                    todo.isCompleted === true ? "completed" : "pending"
+                  }
+                >
+                  {todo.title}
+                </label>
               </div>
-              <label
-                htmlFor="note2"
-                className={
-                  todo.status === "completed" ? "completed" : "pending"
-                }
-              >
-                {todo.title}
-              </label>
+              <div className="input-remove">
+                <input
+                  type="date"
+                  value={todo.startDate}
+                  onChange={(e) => handleChangeDate(todo.title, e.target.value)}
+                />
+                <button
+                  className="remove-btn"
+                  onClick={() => handleRemoveTodo(todo.title)}
+                >
+                  REMOVE
+                </button>
+              </div>
             </div>
+
             <hr className="border" />
           </div>
         ))}
       </section>
+
       {todos.length < 1 && (
         <div className="empty">
           <img src={emptyState} />
@@ -108,14 +186,14 @@ function App() {
             value={textInput}
             onChange={(event) => setTextInput(event.target.value)}
           />
-          <div>
+          {/* <div>
             <h2>Time</h2>
             <input
               type="time"
               value={time}
               onChange={(event) => setTime(event.target.value)}
             />
-          </div>
+          </div> */}
           <div className="modal-btn">
             <button className="cancel-btn" onClick={() => handleCloseModal()}>
               CANCEL
@@ -126,9 +204,16 @@ function App() {
           </div>
         </div>
       </CommonModal>
-      <button className="floating-btn" onClick={handleOpenModal}>
-        +
-      </button>
+      <div className="clear-btn">
+        {todos.length > 0 && (
+          <button className="task-btn" onClick={handleRemoveAllTodos}>
+            CLEAR TASK
+          </button>
+        )}
+        <button className="floating-btn" onClick={handleOpenModal}>
+          +
+        </button>
+      </div>
     </main>
   );
 }
